4c795d5c590c18a447b837349ce7c31512342711
xuxiu <XuxiuSeaKing@163.com>
2020-04-20 10:58:36 +0800

    小王增加

--- src/com/offcn/pay/payService.java
+++ src/com/offcn/pay/payService.java
@@ -1,7 +1,14 @@
 package com.offcn.pay;
 
 public class payService {
+    /**
+     * 小李增加的for循环
+     *
+     */
     public void test(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
         System.out.println("hh");
     }
 }
